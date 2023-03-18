ed79b54233d8b7e1c9e5391e0a9c1b6bb62d5a14
varnal26 <97325704+varnal26@users.noreply.github.com>
2023-03-18 13:53:29 -0500

    Update coverage.sv
    Coverage update

--- coverage.sv
+++ coverage.sv
@@ -5,30 +5,111 @@ class coverage extends uvm_subscriber#(write_xtn);
   virtual parallel_processor_if.MONITOR2_MP vif;
         parallel_processor_config m_cfg;
 	write_xtn  xtn;
-	real cov;
+	real cov,cov1,cov2,cov3;
   
   byte unsigned A, B;
   bit [3:0]OPCODE;
-  //operation_t op_set;*/
+  
 
- covergroup op_cov1;
+ covergroup all_op_cov;
   option.per_instance = 1;
-    coverpoint vif.OPCODE[0] {
-      bins single_cycle[] = {[4'b0001:4'b0011], 4'b0000};
-      bins multi_cycle = {[4'b0100:4'b0111]};
-      bins load_store = {[4'b1000:4'b1001]}; 
-      bins add = {4'b0001};
+    coverpoint vif.monitor1_cb.OPCODE[0] {
+      bins single_cycle[] = {[0:3], [10:15]};
+      bins multi_cycle[] = {[4:7]};
+      bins load_store[] = {[8:9]}; 
 }
 
-endgroup:op_cov1
+coverpoint vif.monitor1_cb.OPCODE[1] {
+      bins single_cycle[] = {[0:3], [10:15]};
+      bins multi_cycle[] = {[4:7]};
+      bins load_store[] = {[8:9]}; 
+}
+
+coverpoint vif.monitor1_cb.OPCODE[2] {
+      bins single_cycle[] = {[0:3], [10:15]};
+      bins multi_cycle[] = {[4:7]};
+      bins load_store[] = {[8:9]}; 
+}
+
+coverpoint vif.monitor1_cb.OPCODE[3] {
+      bins single_cycle[] = {[0:3], [10:15]};
+      bins multi_cycle[] = {[4:7]};
+      bins load_store[] = {[8:9]};  
+}
+
+endgroup:all_op_cov
+
+covergroup op_trans_cov;
+  option.per_instance = 1;
+    coverpoint vif.monitor1_cb.OPCODE[0] {
+      bins singletomult = ([0:3], [10:15] => [4:7]);
+      bins multtosingle = ([4:7] => [0:3], [10:15]);
+      bins multb2b = ([4:7][->2]);
+}
+    coverpoint vif.monitor1_cb.OPCODE[1] {
+      bins singletomult = ([0:3], [10:15] => [4:7]);
+      bins multtosingle = ([4:7] => [0:3], [10:15]);
+      bins multb2b = ([4:7][->2]);
+}
+    coverpoint vif.monitor1_cb.OPCODE[2] {
+      bins singletomult = ([0:3], [10:15] => [4:7]);
+      bins multtosingle = ([4:7] => [0:3], [10:15]);
+      bins multb2b = ([4:7][->2]);
+}
+    coverpoint vif.monitor1_cb.OPCODE[3] {
+      bins singletomult = ([0:3], [10:15] => [4:7]);
+      bins multtosingle = ([4:7] => [0:3], [10:15]);
+      bins multb2b = ([4:7][->2]);
+      bins singleb2b = ([0:3], [10:15][->2]);
+}
+
+endgroup:op_trans_cov
+
+covergroup load_store_cov;
+   coverpoint vif.monitor1_cb.OPCODE[0] {
+	bins load2load = (8 =>8);
+	bins store2store = (9 => 9);
+	//bins load2store = (8=> 9);
+	//bins store2load = (9 => 8);
+	bins loadstoreb2b = ([8:9][->2]);
+	}
+
+   coverpoint vif.monitor1_cb.OPCODE[1] {
+	bins load2load = (8 =>8);
+	bins store2store = (9 => 9);
+	//bins load2store = (8=> 9);
+	//bins store2load = (9 => 8);
+	bins loadstoreb2b = ([8:9][->2]);
+	}
+
+   coverpoint vif.monitor1_cb.OPCODE[2] {
+	bins load2load = (8 =>8);
+	bins store2store = (9 => 9);
+	//bins load2store = (8=> 9);
+	//bins store2load = (9 => 8);
+	bins loadstoreb2b = ([8:9][->2]);
+	}
+
+   coverpoint vif.monitor1_cb.OPCODE[3] {
+	bins load2load = (8 =>8);
+	bins store2store = (9 => 9);
+	//bins load2store = (8=> 9);
+	//bins store2load = (9 => 8);
+	bins loadstoreb2b = ([8:9][->2]);
+	}
+endgroup : load_store_cov 
+
+
 
   
   function new(string name, uvm_component parent);
     super.new(name, parent);
     
-    op_cov1 = new();
-	
-    //zeros_or_ones_on_ops = new();
+    all_op_cov = new();
+    op_trans_cov = new();
+    load_store_cov = new();
+    //cg =new();
+    
   endfunction : new
   
   function void build_phase (uvm_phase phase);
@@ -51,14 +132,20 @@ function void write(write_xtn t);
 
 task run_phase(uvm_phase phase);
     forever begin
-      @( vif.clock);
-	op_cov1.sample();
+      @( vif.monitor1_cb);
+	all_op_cov.sample();
+	op_trans_cov.sample();
+	load_store_cov.sample();
+	//cg.sample();
 	end
 endtask
 
 function void extract_phase(uvm_phase phase);
     super.extract_phase(phase);
-    cov=op_cov1.get_coverage();
+    cov=all_op_cov.get_coverage();
+    cov1=op_trans_cov.get_coverage();
+    cov2=load_store_cov.get_coverage();
+    //cov3=cg.get_coverage();
   endfunction
 
 
@@ -70,109 +157,3 @@ function void report_phase(uvm_phase phase);
 
 endclass
  
-
-
-
-
-
-
-
-
-
-
-
-/*function void connect_phase(uvm_phase phase);
-          vif = m_cfg.vif;
-endfunction
-
-
-
-
-
-          
-  task run_phase(uvm_phase phase);
-    forever begin
-      @( vif.monitor1_cb);
-	A = vif.monitor1_cb.A[0];
-	B  = vif.monitor1_cb.B[0];
-	OPCODE = vif.monitor1_cb.OPCODE[0];
-	
-	//op = new();
-      op_cov.sample();
-//      zeros_or_ones_on_ops.sample();
-    end //forever
-  endtask : run_phase
-  
-  covergroup op_cov;
-  
-    coverpoint vif.monitor1_cb.OPCODE[0] {
-      bins single_cycle[] = {[4'b0001:4'b0011], 4'b0000};
-      bins multi_cycle = {[4'b0100:4'b0111]};
-      
-      bins open_rst[] = ([add_op:mul_op] => rst_op);
-      bins rst_opn[] = (rst_op => [add_op:mul_op]);
-      
-      bins sngl_mul[] = ([add_op:xor_op], no_op => mul_op);
-      bins mul_sngl[] = (mul_op => [add_op:xor_op], no_op);
-      
-      bins twoops[] = ([add_op:mul_op] [* 2]);
-      bins manymult = (mul_op [* 3:5]);
-    }
-  //endgroup:op_cov
-//op_cov op;
-  
-   covergroup zeros_or_ones_on_ops;
-
-      all_ops : coverpoint op_set {
-         ignore_bins null_ops = {rst_op, no_op};}
-
-      a_leg: coverpoint A {
-         bins zeros = {'h00};
-         bins others= {['h01:'hFE]};
-         bins ones  = {'hFF};
-      }
-
-      b_leg: coverpoint B {
-         bins zeros = {'h00};
-         bins others= {['h01:'hFE]};
-         bins ones  = {'hFF};
-      }
-
-      op_00_FF:  cross a_leg, b_leg, all_ops {
-         bins add_00 = binsof (all_ops) intersect {add_op} &&
-                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
-
-         bins add_FF = binsof (all_ops) intersect {add_op} &&
-                       (binsof (a_leg.ones) || binsof (b_leg.ones));
-
-         bins and_00 = binsof (all_ops) intersect {and_op} &&
-                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
-
-         bins and_FF = binsof (all_ops) intersect {and_op} &&
-                       (binsof (a_leg.ones) || binsof (b_leg.ones));
-
-         bins xor_00 = binsof (all_ops) intersect {xor_op} &&
-                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
-
-         bins xor_FF = binsof (all_ops) intersect {xor_op} &&
-                       (binsof (a_leg.ones) || binsof (b_leg.ones));
-
-         bins mul_00 = binsof (all_ops) intersect {mul_op} &&
-                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
-
-         bins mul_FF = binsof (all_ops) intersect {mul_op} &&
-                       (binsof (a_leg.ones) || binsof (b_leg.ones));
-
-         bins mul_max = binsof (all_ops) intersect {mul_op} &&
-                        (binsof (a_leg.ones) && binsof (b_leg.ones));
-
-         ignore_bins others_only =
-                                  binsof(a_leg.others) && binsof(b_leg.others);
-
-      }
-
-	endgroup
-
-//op_cov op;
-  
-endclass : coverage*/
